9ab2c50ab9646fd8ca5f11ad81b47d6ce18743e5
Shakeel Saleemi <shakahs@gmail.com>
2018-02-02 23:11:30 -0800

    Tool now reducing results to 1 global lowest price

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	//"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -20,24 +20,44 @@ func parseTime(layout, value string) *time.Time {
 }
 
 var wg sync.WaitGroup
+var resultChan = make(chan ec2.SpotPrice)
+var lowestSeen ec2.SpotPrice
+
+func determinePrice() {
+	for {
+		select {
+		case res := <-resultChan:
+			if lowestSeen.SpotPrice != nil {
+				lowestSeenPrice, err := strconv.ParseFloat(*lowestSeen.SpotPrice, 32)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				thisPrice, err := strconv.ParseFloat(*res.SpotPrice, 32)
+				if err != nil {
+					fmt.Println(err)
+				}
+				if thisPrice < lowestSeenPrice {
+					lowestSeen = res
+				}
+			} else {
+				lowestSeen = res
+			}
+		}
+	}
+}
 
 func getPrices(region string) {
 	defer wg.Done()
-	fmt.Println(region)
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
-		//Credentials: credentials.NewStaticCredentials("ASIAI4JRMRV6S73MHELQ", "bmDL5BjEVWnRJHIY0szP7HRn0gbkgQfCKrGcWZQG", "FQoDYXdzEFYaDJ9V/+srTMy5OD4jcSKsAX26PMwtKZvHuYWEE43Kii0hTHYrIM/DEE9jOqoihpZG5caSDECFDRct9ar/+Fw7Kq8MlEtb8QKTHvNbFH0EDgZxEF9ggiDKOWFWKoI/GAC2Fu513ZWFuSS3+QbSCxNR3u5bYqFlHcghMqbQtF+lSy13JBkiYaE2IBDgvt/UPsNsy5CqRlBC7Ni+RK8Rcn70ux9IokTPCDfhMb108klrstM9v4Is09ARCvC13kso44HV0wU="),
 	},
 	)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	//creds := stscreds.NewCredentials(sess, "arn:aws:iam::130150402265:user/ec2spotpricechecker")
-
 	svc := ec2.New(sess)
-	//svc := ec2.New(sess, &aws.Config{Credentials: creds})
-	//svc := ec2.New(session.New())
 	input := &ec2.DescribeSpotPriceHistoryInput{
 		InstanceTypes: []*string{
 			aws.String("p3.2xlarge"),
@@ -45,8 +65,6 @@ func getPrices(region string) {
 		ProductDescriptions: []*string{
 			aws.String("Linux/UNIX (Amazon VPC)"),
 		},
-		//EndTime:   parseTime("2006-01-02T15:04:05Z", "2014-01-06T08:09:10Z"),
-		//StartTime: parseTime("2006-01-02T15:04:05Z", "2014-01-06T07:08:09Z"),
 	}
 
 	result, err := svc.DescribeSpotPriceHistory(input)
@@ -66,10 +84,10 @@ func getPrices(region string) {
 	}
 
 	seen := make(map[string]bool)
-	//fmt.Print(result)
 	for _, val := range result.SpotPriceHistory {
 		if seen[*val.AvailabilityZone] == false {
 			fmt.Println(val)
+			resultChan <- *val
 			seen[*val.AvailabilityZone] = true
 		}
 	}
@@ -81,12 +99,12 @@ func main() {
 		"sa-east-1",
 		"eu-west-1", "eu-west-2", "eu-west-3", "eu-central-1",
 		"ap-south-1", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2"}
-	//fmt.Println(regionList)
 
+	go determinePrice()
 	for _, r := range regionList {
-		//fmt.Println(r)
 		wg.Add(1)
 		go getPrices(r)
 	}
 	wg.Wait()
+	fmt.Println(lowestSeen)
 }
